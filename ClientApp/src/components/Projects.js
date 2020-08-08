cfbf3c3c240b7b620de39f2f7ec4757c1c9a0162
Elliot DeMatteis <elliotdematteis@gmail.com>
2020-08-08 00:16:49 -0700

    Update projects

--- ClientApp/src/components/Projects.js
+++ ClientApp/src/components/Projects.js
@@ -25,6 +25,21 @@ export class Projects extends Component {
                             <small className="text-muted"><a href="https://opensync.live" target="_blank" rel="noopener noreferrer">opensync.live</a></small>
                         </div>
                     </div>
+                    <div className="list-group-item flex-column align-items-start">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">Orbit</h5>
+                            <small><a href="https://github.com/brasky/pythonorbit" target="_blank" rel="noopener noreferrer">github</a></small>
+                        </div>
+                        <p className="mb-1">Orbit is a bot designed to take advantage of inefficiencies in cryptocurrency markets using a strategy called triangular arbitrage.
+                            Originally private and being run with some success in 2017/2018, it's no longer being run/developed and thus has been made public. I do not recommend running it with any amount of money on the line...</p>
+                    </div>
+                    <div className="list-group-item flex-column align-items-start">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">TriArbMarkets</h5>
+                            <small><a href="https://github.com/OrbitGroup/" target="_blank" rel="noopener noreferrer">github</a></small>
+                        </div>
+                        <p className="mb-1">TriArbMarkets is an in-progress project which aims to identify triangular arbitrage opportunities on cryptocurrency exchanges and visualize them clearly.</p>
+                    </div>
                 </div>
 
             </div>
